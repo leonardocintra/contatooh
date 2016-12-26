16d64b0c850e5a4666b2c6c0cc5da8dfbf9ad1d1
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-26 21:46:56 -0200

    implementando o express-load

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var home = require('../app/routes/home');
+var load = require('express-load');
+
 
 module.exports = function() {
     var app = express();
@@ -12,7 +13,17 @@ module.exports = function() {
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
-    home(app);
+
+    /*
+    Pasta padrão
+        O parâmetro {cwd: ‘app’} foi necessário para mudar o diretório
+    padrão, pois a função procura as pastas no diretório raiz contatooh e
+    precisamos que ela considere a pasta contatooh/app .
+    */
+    load('models', {cwd: 'app'})
+        .then('controllers')
+        .then('routes')
+        .into(app);
 
     return app;
 };
