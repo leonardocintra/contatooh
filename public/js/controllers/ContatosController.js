049c867020a5b6d949c63de0786ab66e4cf078f8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-28 12:31:23 -0200

    angular ok

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -4,4 +4,16 @@ angular.module('contatooh').controller('ContatosController',
         $scope.incrementa = function() {
             $scope.total++;
         };
-});
+
+        $scope.contatos = [
+            {_id: 1, nome: 'Ronaldo', email: 'exemplocontato1@gmail.com'},
+            {_id: 2, nome: 'Juliana', email: 'exemplocontato2@gmail.com'},
+            {_id: 3, nome: 'Sara', email: 'exemplocontato3@gmail.com'},
+            {_id: 4, nome: 'Humberto', email: 'exemplocontato4@gmail.com'},
+            {_id: 5, nome: 'Diego', email: 'exemplocontato5@gmail.com'},
+
+        ];
+
+        $scope.filtro = "";
+    }
+);
