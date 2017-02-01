bebcd2dcfc6275d3dfd1221fa184335032110df6
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-01 16:17:42 -0200

    configurado informacoes de database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,22 @@
 (function () {
 
     const mongoose = require('mongoose')
+
     module.exports = function(uri) {
-        mongoose.connect(uri)
+        
+        mongoose.connect(uri, {server: { poolSize: 15}})
+
+        mongoose.connection.on('connected', () => console.log(`Mongoose! Conectado em ${uri}`))
+        mongoose.connection.on('disconnected', () => console.log(`Mongoose! Desconectado de ${uri}`))
+        mongoose.connection.on('error', () => console.log(`Mongoose! Erro na conexão: ${uri}`))
+
+        process.on('SIGINT', () => {
+            mongoose.connection.close(() => {
+                console.log('Mongoose! Desconectado pelo terminio da aplicação.')
+                process.exit(0)
+            })
+        })
+
     }
 
 })()
