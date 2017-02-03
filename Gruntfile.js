03f1131b0568925186e2aadb9519fa06a5aa0678
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-03 20:04:36 -0200

    erro para gerar grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,17 +11,38 @@
                     dest: 'dist'
                 }
             },
+
             clean: {
                 dist: {
                     src: 'dist'
                 }
+            },
+
+            usemin: {
+                html: 'dist/app/views/**/*.ejs'
+            },
+
+            useminPrepare: {
+                options: {
+                    root: 'dist/public',
+                    dest: 'dist/public'
+                },
+                html: 'dist/app/views/**/*.ejs'
             }
+
+
         })
 
-        grunt.registerTask('default', ['dist'])
+        grunt.registerTask('default', ['dist', 'minifica'])
         grunt.registerTask('dist', ['clean', 'copy'])
+        grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin'])
+
         grunt.loadNpmTasks('grunt-contrib-copy')
         grunt.loadNpmTasks('grunt-contrib-clean')
+        grunt.loadNpmTasks('grunt-contrib-concat')
+        grunt.loadNpmTasks('grunt-contrib-uglify')
+        grunt.loadNpmTasks('grunt-contrib-cssmin')
+        grunt.loadNpmTasks('grunt-usemin')
     }
 
 })()
